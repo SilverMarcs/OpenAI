36e123cbc17ad6a8b124ed339c72b9bb80ed6786
Zabir Raihan <zabirraihan6@gmail.com>
2024-07-25 19:50:10 +0800

    models endpoint compatible with thrid party openai providers

--- Sources/OpenAI/Public/Models/Models/ModelResult.swift
+++ Sources/OpenAI/Public/Models/Models/ModelResult.swift
@@ -9,18 +9,39 @@ import Foundation
 
 /// The model object matching the specified ID.
 public struct ModelResult: Codable, Equatable {
-
     /// The model identifier, which can be referenced in the API endpoints.
     public let id: String
+    /// The proper name of model
+    public let name: String
     /// The Unix timestamp (in seconds) when the model was created.
-    public let created: TimeInterval
+    public let created: TimeInterval?
     /// The object type, which is always "model".
-    public let object: String
+    public let object: String?
     /// The organization that owns the model.
-    public let ownedBy: String
+    public let ownedBy: String?
+    
+    // Custom initializer to handle decoding
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? self.id.capitalized
+        self.created = try container.decodeIfPresent(TimeInterval.self, forKey: .created)
+        self.object = try container.decodeIfPresent(String.self, forKey: .object)
+        self.ownedBy = try container.decodeIfPresent(String.self, forKey: .ownedBy)
+    }
 
+    public init(id: String, name: String?, created: TimeInterval?, object: String?, ownedBy: String?) {
+        self.id = id
+        self.name = name ?? id.capitalized
+        self.created = created
+        self.object = object
+        self.ownedBy = ownedBy
+    }
+
+    // Coding keys to map the JSON keys to the struct properties
     public enum CodingKeys: String, CodingKey {
         case id
+        case name
         case created
         case object
         case ownedBy = "owned_by"
--- Sources/OpenAI/Public/Models/Models/ModelsResult.swift
+++ Sources/OpenAI/Public/Models/Models/ModelsResult.swift
@@ -9,9 +9,25 @@ import Foundation
 
 /// A list of model objects.
 public struct ModelsResult: Codable, Equatable {
-
     /// A list of model objects.
     public let data: [ModelResult]
-    /// The object type, which is always `list`
-    public let object: String
+    // The object type, which is always `list`
+    public let object: String?
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.data = try container.decode([ModelResult].self, forKey: .data)
+        self.object = try container.decodeIfPresent(String.self, forKey: .object)
+    }
+
+    public init(data: [ModelResult], object: String?) {
+        self.data = data
+        self.object = object
+    }
+
+    public enum CodingKeys: String, CodingKey {
+        case data
+        case object
+    }
 }
+
